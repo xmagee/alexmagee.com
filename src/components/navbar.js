7a1de710a24055637e3abb3332d2bfaa9de5e932
Alex Magee <86931568+xmagee@users.noreply.github.com>
2021-07-18 22:52:07 -0400

    Added rel to links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,10 @@ export default function Navbar() {
     return (
         <nav className='links_container'>
             {nav_links.map((i, index) => ( 
-                <span key={index}><a href={i[1]} target='_blank'>{i[0]}</a> {index !==  nav_links.length - 1 ? ' | ' : ''}</span> 
+                <span key={index}>
+                    <a href={i[1]} target='_blank' rel='noopener'>{i[0]}</a>
+                    {index !==  nav_links.length - 1 ? ' | ' : ''}
+                </span> 
             ))}
         </nav>
     )
